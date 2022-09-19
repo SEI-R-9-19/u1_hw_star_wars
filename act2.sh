471987bca6b0e24117ba78d2102fd0dad4572e97
jeremyHarrell <hillvulf@gmail.com>
2022-09-19 17:11:41 -0500

    Act 2 and Act 3 final

--- act2.sh
+++ act2.sh
@@ -5,9 +5,11 @@ touch obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
 touch luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-mkdir millienium_falcon
+mkdir millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
 touch han_solo.txt chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
 mv luke_skywalker.txt obi_wan.txt princess_leia.txt millienium_falcon 
 # 7. Move the millenium_falcon into the death_star.
+ mv millenium_falcon .. 
+ mv millenium_falcon empire/death_star 
