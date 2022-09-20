28c079faa9e7074dbc5ac311c19076f79a7a8693
Julia Lima <julia.manvailer17@icloud.com>
2022-09-20 09:26:16 -0400

    act3

--- act3.sh
+++ act3.sh
@@ -3,12 +3,12 @@
 mv  millenium_falcon u1_hw_star_wars/star_wars/empire/death_star  
 
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-rm obi_wan.txt
+rmf obi_wan.txt
 # 3.  Our heroes have disabled the tractor beam! Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
 mv death_star/* millenium_falcon
 # 4. Move the millenium_falcon back into the rebellion directory.
 mv millenium_falcon rebellion
 # 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star into the empire directory!
-mv darth_vader empire
+mv darth_vader u1_hw_star_wars/star_wars/empire
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
