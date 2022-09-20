31a13c83df23e64beff208aea6c2c8f50da2713a
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-09-20 09:17:18 -0400

    make changes to index.html

--- act3.sh
+++ act3.sh
@@ -1,13 +1,16 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
-# Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
-
+# Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all fders inside of a directory (directory/*)iles/fol
+mv millenium_falcon/* ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/empire/death_star
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-
+rm obi-wan.txt
 # 3.  Our heroes have disabled the tractor beam! Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
-
+mv millenium_falcon ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/rebellion
+mv death_star/* ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/rebellion/millenium_falcon
+mv darth_vadar.txt ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/empire/death_star
 # 4. Move the millenium_falcon back into the rebellion directory.
-
+mv millenium_falcon ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/rebellion
 # 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star into the empire directory!
-
+mv darth_vadar.txt ~/ga_seir/unit1/homework/u1_hw_star_wars/star_wars/empire/
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
+rm -rf death_star
