8e548c23cd9d0858bdc697db5425e50c0f87fc53
Marissa Dilig <hello.maripd@gmail.com>
2022-09-20 09:13:24 -0500

    homework1 2/3 complete

--- act3.sh
+++ act3.sh
@@ -1,4 +1,5 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
+
 # Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
 
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
