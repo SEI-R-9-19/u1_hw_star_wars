3469efd129974761bff53c37351e92245c40e288
Mohamed Osman <osman.mohamed2244@gmail.com>
2022-09-26 14:13:57 -0500

    changed act1, act2, act3 created 6 new files

--- act2.sh
+++ act2.sh
@@ -1,13 +1,16 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND using &&) create a file called princess_leia.txt with the text "Help me, Obi-Wan…"
-
+ touch princess.leia.txt && echo "Help me, Obi-Wan.." >> princess.leia.txt
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+touch obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+touch luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+touch han_solo.txt && touch chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+mv ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/luke_skywalker.txt ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/millenium_falcon
+mv ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/obi_wan.txt ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/millenium_falcon
+mv ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/princess.leia.txt ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/rebellion/millenium_falcon ~/ga_seir/Unit_1/Homework/u1_hw_star_wars/star_wars/empire/death_star
